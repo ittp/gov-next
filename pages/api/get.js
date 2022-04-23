a2329f5f5d2276106f4e6d659fa462fd6bf0417a
ittp <ittp.spb@gmail.com>
2022-04-23 06:55:25 +0300

    5 files modified

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,7 +1,24 @@
-const apps = {};
+const axios = require('axios');
+
+const { YandexDisk } = require('yandex-disk');
+
+// let [ res1, res2 ] = await Promise.all([
+//   fetch(url_1),
+//   fetch(url_2)
+// ]);
+const os = require('os');
+let net = os.networkInterfaces();
 
 export default (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   // debugger;
-  res.status(200).json({ apps });
+  console.log(1);
+  res.status(200);
+  res.json({
+    data: 1,
+    net,
+    os: {
+      ...req.headers,
+    },
+  });
 };
--- pages/index.js
+++ pages/index.js
@@ -2,41 +2,61 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import React, { Component, useState } from 'react';
 
-import { Table, Form, Select, Input, InputNumber, Button } from 'antd';
+import {
+  Table,
+  Form,
+  Select,
+  Input,
+  InputNumber,
+  Button,
+  List,
+  Row,
+  Col,
+  Layout,
+} from 'antd';
 import 'antd/dist/antd.css';
+import TextArea from 'antd/lib/input/TextArea';
 
+const axios = require('axios');
 const { Option } = Select;
-
+const os = require('os');
 class SelectForm extends Component {
   state = {
-    browser: {},
-    os: {},
-    arch: {},
-    version: {},
+    browser: [
+      {
+        title: 'Chrome',
+        value: 'chrome',
+        name: 'chromium-gost',
+        template: {},
+      },
+      { title: 'Yandex', value: 'yandex', name: 'yandex-browser', url: '' },
+      {
+        title: 'Firefox',
+        value: 'Firefox',
+        name: 'firefox-gost',
+        url: '',
+      },
+    ],
   };
 
-  onSelect = async (data) => {
+  onSelect = async (_, data) => {
     console.log(data);
-    alert(1);
+    if (data) {
+      console.log(data);
+      // axios.get('localhost:3000/api/hello');
+    }
   };
   render() {
+    let browser = Object.values(this.state).map((i) => [i.name]);
+    console.log(browser);
+
     return (
-      <Form id="template" layout={'inline'} onSubmit={this.onSelect}>
-        <Select id="browser">
-          <Select.Option value="yandex">Yandex</Select.Option>
-          <Select.Option value="chrome">Chrome</Select.Option>
-          <Select.Option value="firefox"> Firefox</Select.Option>
-        </Select>
-
-        <Select id={'os'} onChange={(data) => console.log(data)}>
-          <Select.Option value="windows">Windows</Select.Option>
-          <Select.Option value="linux">Linux</Select.Option>
-        </Select>
-        <Select id={'arch'} onChange={(data) => console.log(data)}>
-          <Select.Option value="32">32</Select.Option>
-          <Select.Option value="64">64</Select.Option>
-        </Select>
-
+      <Form id="config" onSubmit={this.onSelect} state={this.state}>
+        <Select
+          options={this.state.browser}
+          defaultActiveFirstOption={true}
+          onChange={this.onSelect}
+        />
         <Button>Save</Button>
       </Form>
     );
@@ -46,7 +66,7 @@ export default function Home(config) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>gov</title>
+        <title>Config</title>
       </Head>
       <main className={styles.main}>
         <SelectForm />
